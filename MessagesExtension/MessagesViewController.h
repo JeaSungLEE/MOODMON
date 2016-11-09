a5398ba65e76edd9e067abeafae000050f18164d
song.lee <song@ijaeseongs-MacBook-Pro.local>
2016-11-09 19:24:27 +0900

    아이메세지 타겟 생

---
+++ MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by 이재성 on 09/11/2016.
+//  Copyright © 2016 HUB. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
