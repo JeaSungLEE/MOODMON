f99aa9ffc380e474890924dab291e57a687ee1c3
Lee Kyu-Won <lkwon0302@gmail.com>
2016-12-11 16:35:50 +0900

    fRealm

---
+++ Podfile
@@ -0,0 +1,10 @@
+
+
+
+
+platform :ios, '8.0'
+
+target 'moodMon' do
+  use_frameworks!
+  pod 'Realm'
+end
---
+++ Pods/Target Support Files/Pods-moodMon/Pods-moodMon-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_moodMon : NSObject
+@end
+@implementation PodsDummy_Pods_moodMon
+@end
